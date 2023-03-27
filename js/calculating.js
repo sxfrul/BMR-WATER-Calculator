92dafa812cc1a3167718d92ac3fe1fa450a57f1b
sxfrul <muhdsafrul@gmail.com>
2023-03-27 11:31:00 +0800

    Optimized algorithm to check activity dropdown menu

--- js/calculating.js
+++ js/calculating.js
@@ -1,10 +1,10 @@
 function waterCalculator()
 {
         //WATER INTAKE CALCULATIONS
-    var userMass = parseInt(document.getElementById("user-mass").value);
-    var userHeight = parseInt(document.getElementById("user-height").value);
-    var userAge = parseInt(document.getElementById("user-age").value);
-    waterAmount = ((userMass/0.453592)*0.5)*29.5735, 2; // Water intake is stored in waterAmount(var)
+    const userMass = parseFloat(document.getElementById("user-mass").value);
+    const userHeight = parseFloat(document.getElementById("user-height").value);
+    const userAge = parseFloat(document.getElementById("user-age").value);
+    var waterAmount = ((userMass/0.453592)*0.5)*29.5735; // Water intake is stored in waterAmount(var)
 
         //CHECK THE GENDERS -- stores BMR in a variable
     if(document.getElementById("male").checked == true){
@@ -17,21 +17,13 @@ function waterCalculator()
 
     //CHECK REGIME SELECTED
     var userActivity = document.getElementById("activitySelect");
-    if(userActivity.options[ userActivity.selectedIndex ].value == "sedentary"){
-        var BMR = BMR*1.2;
-    } else if(userActivity.options[ userActivity.selectedIndex ].value == "lightly-active"){
-        var BMR = BMR*1.375;
-    } else if(userActivity.options[ userActivity.selectedIndex ].value == "moderately-active"){
-        var BMR = BMR*1.55;
-    } else if(userActivity.options[ userActivity.selectedIndex ].value == "active"){
-        var BMR = BMR*1.725;
-    } else if(userActivity.options[ userActivity.selectedIndex ].value == "very-active"){
-        var BMR = BMR*1.9;}
+    var getDropDownValue = userActivity.options[ userActivity.selectedIndex ].value
+    BMR = BMR * (1.2+(0.175*getDropDownValue))
 
     //IF NOTHING IS SELECTED
     if((document.getElementsByName("genders").checked == true) || (userActivity.options[ userActivity.selectedIndex ].value == "") || (isNaN(BMR))){
         document.getElementById("BMR-output").innerHTML = "Please fill in every details dumbass";
     } else {
-        document.getElementById("BMR-output").innerHTML = "Your BMR is " + Math.round(BMR) + " and you need " + Math.round(waterAmount) + "ml of water per day";}
+        document.getElementById("BMR-output").innerHTML = "Your BMR is " + BMR.toFixed(2) + " and you need " + waterAmount.toFixed(2) + "ml of water per day";}
 
 }
